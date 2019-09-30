8dfe4aac1a1a81f073c59cc4ebaa37ab6a2586d2
marcus quigley <marcusquigley@gmail.com>
2019-09-30 10:53:55 -0400

    init commit

---
+++ Oct19.Study/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Oct19.Study
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            Console.Read(); 
+        }
+                
+        
+    }
+}
