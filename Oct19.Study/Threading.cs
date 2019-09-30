4b0ee2c8b3f01fca872a52bca79c99c102d7d657
marcus quigley <marcusquigley@gmail.com>
2019-09-30 11:00:57 -0400

    Threading init

---
+++ Oct19.Study/Threading.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oct19.Study
+{
+    class Threading
+    {
+    }
+}
--- Test/ThreadingTest.cs
+++ Test/ThreadingTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Test
 {
-    public class UnitTest1
+    public class ThreadingTest
     {
         [Fact]
         public void Test1()
